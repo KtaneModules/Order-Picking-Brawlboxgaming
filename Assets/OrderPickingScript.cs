f66e942a80e14877101543e9dac3a127e0691380
Brawlboxgaming <zombielord343@gmail.com>
2021-08-16 15:34:15 +0100

    Initial commit

---
+++ Assets/OrderPickingScript.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Rnd = UnityEngine.Random;
+using KModkit;
+using System.Text.RegularExpressions;
+
+public class OrderPickingScript : MonoBehaviour
+{
+    public KMBombModule Module;
+    public KMBombInfo BombInfo;
+    public KMAudio Audio;
+    public KMRuleSeedable RuleSeedable;
+
+    private static int _moduleIdCounter = 1;
+    private int _moduleId;
+    private bool _moduleSolved;
+
+    void Start()
+    {
+
+    }
+}
